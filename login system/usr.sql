cbf18bccf28e4c70758bf198e4dc7be541753517
tiago-py <tiago.braga0012@gmail.com>
2023-03-29 20:11:07 -0300

    login

--- login system/usr.sql
+++
@@ -1,7 +0,0 @@
-    create table usr(
-        id int not null,
-        nome varchar(80) not null,
-        email varchar(120) not null,
-        senha int not null,
-        primary key(id)
-    );
