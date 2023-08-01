6aef0fe59bb3e779d41b78d27d16dbf739097e57
tiago-py <tiago.braga0012@gmail.com>
2023-08-01 14:35:56 -0300

    limpando o dataset

---
+++ gas-prices/index.py
@@ -0,0 +1,68 @@
+import dash
+from dash import html, dcc, Input, Output, State
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+from dash_bootstrap_templates import ThemeSwitchAIO
+
+
+# ========= App ============== #
+FONT_AWESOME = ["https://use.fontawesome.com/releases/v5.10.2/css/all.css"]
+dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.4/dbc.min.css"
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY, dbc_css])
+app.scripts.config.serve_locally = True
+server = app.server
+
+# ========== Styles ============ #
+
+template_theme1 = "flatly"
+template_theme2 = "vapor"
+url_theme1 = dbc.themes.FLATLY
+url_theme2 = dbc.themes.VAPOR
+
+
+# ===== Reading n cleaning File ====== #
+df_main = pd.read_csv("data_gas.csv")
+
+df_main['DATA INICIAL'] = pd.to_datetime(df_main['DATA INICIAL'])
+df_main['DATA FINAL'] = pd.to_datetime(df_main['DATA FINAL'])
+
+df_main['DATA MEDIA'] = ((df_main['DATA FINAL']-df_main['DATA INICIAL'])/2)+df_main['DATA INICIAL']
+
+df_main = df_main.sort_values(by='DATA MEDIA', ascending = True)
+df_main.rename(columns = {'DATA MEDIA' : 'DATA'}, inplace = True)
+df_main.rename(columns = {'PREÇO MÉDIO REVENDA ' : 'PRECO REVENDA (R$/L)'}, inplace = True)
+
+df_main["ANO"] = df_main["DATA"].apply(lambda x: str(x.year))
+df_main = df_main[df_main.PRODUTO == 'GASOLINA COMUM']
+
+
+
+
+
+df_main = df_main.reset_index()
+
+
+df_main.drop(['UNIDADE DE MEDIDA','UNIDADE DE MEDIDA','COEF DE VARIAÇÃO DISTRIBUIÇÃO','NÚMERO DE POSTOS PESQUISADOS','DATA FINAL',
+'DATA INICIAL','PREÇO MÁXIMO DISTRIBUIÇÃO','PREÇO MÍNIMO DISTRIBUIÇÃO','MARGEM MÉDIA REVENDA','PREÇO MÁXIMO REVENDA','PREÇO MÍNIMO REVENDA'
+,'PRODUTO', 'PREÇO MÉDIO DISTRIBUIÇÃO' ], inplace = True, axis=1)
+
+df_main.info()
+# =========  Layout  =========== #
+app.layout = dbc.Container(children=[
+    
+
+
+
+
+], fluid=True, style={'height': '100%'})
+
+
+# ======== Callbacks ========== #
+
+
+# Run server
+if __name__ == '__main__':
+    app.run_server(debug=True)
